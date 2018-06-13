c3a92d57642391541b3a1b01444c3cbc79da84fa
Mark Sommers <kacysommers@gmail.com>
2018-06-13 07:46:00 -0700

    added links to main page, a skills page, and a form to add new skill

--- index.js
+++ index.js
@@ -9,14 +9,35 @@ app.use(bp.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/', function(req, res) {
+	// TODO: Add links to index.html so users can navigate
 	res.sendFile(__dirname + '/static/html/index.html');
 });
 
-app.get('/skills', function(req, res) {
-	var skills = fs.readFileSync('data.json');
-	skills = JSON.parse(skills); // turns into object
-	res.send(skills);
+app.get('/form', function(req, res) {
+	res.sendFile(__dirname + '/static/html/form.html');
 });
 
+app.post('/skills/new', function(req, res){
+	var skill = req.body.skill;
+	var level = req.body.level;
+	var skillsArr = fs.readFileSync('./data.json');
+	skillsArr = JSON.parse(skillsArr);
+	skillsArr.push({'name': skill, 'level': level});
+	skillsArr = JSON.stringify(skillsArr);
+	fs.writeFileSync('./data.json', skillsArr);
+	res.redirect('/skills');
+});
+
+app.get('/skills', function(req, res) {
+	var skills = fs.readFileSync('./data.json');
+	skills = JSON.parse(skills); // turns into object
+	// TODO: Render this array of skills into an EJS template
+	res.render('skills', {skills: skills});
+});
+
+// TODO: Add GET route that returns static page containing a form
+
+// TODO: Add POST route that writes new skill to the file, redirects to skills index
+// TODO: Form should have action='/skills' and method='POST'
 
 app.listen(process.env.PORT || 3000);
